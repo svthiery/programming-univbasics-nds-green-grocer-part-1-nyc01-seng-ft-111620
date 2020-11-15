215566d2e0d909ba12d3614dd1138594ee14163c
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-15 16:53:08 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,8 +11,8 @@ def consolidate_cart(cart)
   cart.map do |item|
     item[:count] = 1    
   end
-  
   cart.each do |item|
+    binding.pry
     if new_cart.length == 0 
       new_cart << item
     elsif find_item_by_name_in_collection(item[:item], new_cart) == false
