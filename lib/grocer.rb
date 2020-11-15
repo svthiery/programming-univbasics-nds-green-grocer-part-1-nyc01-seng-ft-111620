2df76a3170f6aa1f12a93d60fc9d2ce1ecf3864f
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-15 16:48:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,9 +19,9 @@ def consolidate_cart(cart)
     if find_item_by_name_in_collection(item[:item], new_cart) == false
       new_cart << item
     else 
-      binding.pry
       item_index = new_cart.index(find_item_by_name_in_collection(item[:item], new_cart))
       new_cart[item_index][:count] += 1 
+      binding.pry
     end
   end
   return new_cart
